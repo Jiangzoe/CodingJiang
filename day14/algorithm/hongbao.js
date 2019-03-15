30ce8f690938aaf13153f5fc993f2172ab957c11
Jiangzoe <1097288257@qq.com>
2019-03-15 09:59:13 +0800

    algorithm之红包算法+weui+css3之animation&transition属性

---
+++ day14/algorithm/hongbao.js
@@ -0,0 +1,18 @@
+function hongbao(total,num){
+    const arr = [];
+    let restAmount = total; //余额初始为总金额
+    let restNum = num ;
+
+    for(let i = 0; i<num-1;i++){   //余一个人不发
+        // 平均值的两倍
+        let amount = parseFloat(Math.random()*(restAmount/restNum*2)).toFixed(2);
+        restAmount -= amount;
+        restNum--;
+        arr.push(amount);
+    }
+    // 最后一个人restAmount
+    arr.push(restAmount.toFixed(2));
+    return arr;
+}
+
+console.log(hongbao(20,44));
