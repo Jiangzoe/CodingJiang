2f6cd4b8809047b9b814b94a6a71fa713a69f0fd
Jiangzoe <1097288257@qq.com>
2019-06-11 12:28:24 +0800

    iview

---
+++ day53/iview/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import Index from'@/components/detail/Index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/table',
+      name: 'Home',
+      component: Home,
+      children:[
+       { path:'/',    
+        component:Index
+      }
+      ]
+    }
+  ]
+})
